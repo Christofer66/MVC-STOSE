32899ddcb2bc3bbf293fd6a6a526d5759264bcd2
Anderson Mendoza <davidmramos53@gmail.com>
2022-10-26 11:29:31 -0500

    reutilizando los controlers para index

--- Certus01/src/main/java/com/example/demo/controller/indexController.java
+++ Certus01/src/main/java/com/example/demo/controller/indexController.java
@@ -9,9 +9,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
-import com.example.demo.models.indexModel;
+import com.example.demo.models.Lista;
 
 
 
@@ -26,10 +25,21 @@ public class indexController {
 
 	@GetMapping({ "/home", "/inicio", "/", "/Home", "Inicio" })
 	public String ControllerIndex(Model model) {
-        
-        //Libros y categorias
 		
-		indexModel LibrosC = new indexModel();
+		//Ayuda y contacto
+		Lista AyudaC = new Lista();
+		AyudaC.setCuenta("Tu Cuenta");
+		AyudaC.setPedidos("Tus Pedidos");
+		AyudaC.setEnvios("Tarifas de Envio");
+		AyudaC.setDevoluciones("Devoluciones");
+		AyudaC.setContactanos("Contactanos");
+		
+		
+		List<Lista> ListAyudaC = new ArrayList<>();
+		ListAyudaC.add(AyudaC);
+
+        //Libros y categorias
+		Lista LibrosC = new Lista();
 		LibrosC.setArte("Arte");
 		LibrosC.setBiografias("Biografias");
 		LibrosC.setCiencias("Ciencias");
@@ -42,10 +52,34 @@ public class indexController {
 		LibrosC.setFilosofiaYreligion("Filosofia y Religion");
 		LibrosC.setInfantilesYjuveniles("Infantiles y Juveniles");
 
-        List<indexModel> ListLibrosC = new ArrayList<>();
+        List<Lista> ListLibrosC = new ArrayList<>();
 		ListLibrosC.add(LibrosC);
+
+		//Metodos de pago
+		Lista MetodosP = new Lista();
+		MetodosP.setTarjetasDeCredito("Tarjetas de Credito");
+		MetodosP.setPaypal("Paypal");
+		
+		List<Lista> ListMetodosP = new ArrayList<>();
+		ListMetodosP.add(MetodosP);
+
+		//Sobre Nosotros
+		Lista Nosotros = new Lista();
+		Nosotros.setIempresa("Informacion de la Compañia");
+		Nosotros.setNoticias("Noticias");
+		Nosotros.setInversores("Inversores");
+		Nosotros.setPolíticas("Politicas");
+		Nosotros.setClienteV("Valoracion de los clientes");
+		
+		List<Lista> ListNosotros = new ArrayList<>();
+		ListNosotros.add(Nosotros);
+
         
         model.addAttribute("TituloPagina", titlePage);
+		model.addAttribute("AyudaC", ListAyudaC);	
+		model.addAttribute("LibrosC", ListLibrosC);
+		model.addAttribute("MetodosP", ListMetodosP);
+		model.addAttribute("Nosotros", ListNosotros);
 
         return "index";
     }
