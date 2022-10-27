3b8cb6263050baac3ff620bb9ec17d1b86741c4d
Jankarurosu <103855697+Jankarurosu@users.noreply.github.com>
2022-10-27 17:30:55 -0500

    Avance de detalle libro

--- Certus01/src/main/java/com/example/demo/controller/DetalleLibroController.java
+++ Certus01/src/main/java/com/example/demo/controller/DetalleLibroController.java
@@ -1,9 +1,16 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.models.indexModel;
+import com.example.demo.models.tiendaModel;
 
 @Controller
 @RequestMapping("/app")
@@ -13,8 +20,82 @@ public class DetalleLibroController {
 	private String titlePage;
 	
 	@GetMapping({"/detalle-libro", "/Detalle-Libro"})
-	public String DetalleLibro() {
+	public String DetalleLibro(Model model) {
 		
+		//SECCION detalle-libros
+		//Imagen de libro grande
+		
+		indexModel ImagenLibroG = new indexModel();
+		
+		List<indexModel> ListImagenLibroG = new ArrayList<>();
+		ListImagenLibroG.add(ImagenLibroG);
+		
+		//SECCION libros relacionados
+		//Libros relacionados
+		
+		indexModel LibroR = new indexModel();
+		
+		List<indexModel> ListLibroR = new ArrayList<>();
+		ListLibroR.add(LibroR);
+		
+		//CONTENIDO DEL FOOTER
+		//Ayuda y contacto
+		tiendaModel AyudaC = new tiendaModel();
+		AyudaC.setCuenta("Tu Cuenta");
+		AyudaC.setPedidos("Tus Pedidos");
+		AyudaC.setEnvios("Tarifas de Envio");
+		AyudaC.setDevoluciones("Devoluciones");
+		AyudaC.setContactanos("Contactanos");
+						
+						
+		List<tiendaModel> ListAyudaC = new ArrayList<>();
+		ListAyudaC.add(AyudaC);
+		
+		//Libros y categorias
+		tiendaModel LibrosC = new tiendaModel();
+		LibrosC.setArte("Arte");
+		LibrosC.setBiografias("Biografias");
+		LibrosC.setCiencias("Ciencias");
+		LibrosC.setComputacion("Computacion");
+		LibrosC.setDeportes("Deportes");
+		LibrosC.setDerecho("Derecho");
+		LibrosC.setEconomia("Economia");
+		LibrosC.setEstilosDeVida("Estilos de Vida");
+		LibrosC.setFiccion("Ficcion");
+	    LibrosC.setFilosofiaYreligion("Filosofia y Religion");
+	    LibrosC.setInfantilesYjuveniles("Infantiles y Juveniles");
+		
+		List<tiendaModel> ListLibrosC = new ArrayList<>();
+		ListLibrosC.add(LibrosC);
+		
+		//Metodos de pago
+	    tiendaModel MetodosP = new tiendaModel();
+	    MetodosP.setTarjetasDeCredito("Tarjetas de Credito");
+	    MetodosP.setPaypal("Paypal");
+						
+	    List<tiendaModel> ListMetodosP = new ArrayList<>();
+	    ListMetodosP.add(MetodosP);
+		
+	    //Sobre Nosotros
+	    tiendaModel Nosotros = new tiendaModel();
+	    Nosotros.setIempresa("Informacion de la Compañia");
+	    Nosotros.setNoticias("Noticias");
+	    Nosotros.setInversores("Inversores");
+	    Nosotros.setPoliticas("Politicas");
+	    Nosotros.setClienteV("Valoracion de los clientes");
+						
+	    List<tiendaModel> ListNosotros = new ArrayList<>();
+	    ListNosotros.add(Nosotros);
+		
+	    //Models 
+	    model.addAttribute("TituloPagina", titlePage);
+	    model.addAttribute("AyudaC", ListAyudaC);	
+	    model.addAttribute("LibrosC", ListLibrosC);
+	    model.addAttribute("MetodosP", ListMetodosP);
+	    model.addAttribute("Nosotros", ListNosotros);
+	    model.addAttribute("ImagenLibroG", ListImagenLibroG);
+	    model.addAttribute("LibroR", ListLibroR);
+	    
 		return "detalle-libro";
 	}
 }
---
+++ Certus01/src/main/java/com/example/demo/models/detalleLibroModel.java
@@ -0,0 +1,84 @@
+package com.example.demo.models;
+
+public class detalleLibroModel {
+	
+	//Imagen de libro grande
+	private String ImagenLibroG;
+	
+	//Libros relacionados
+	private String LibroRelacionado1;
+	private String LibroRelacionado2;
+	private String LibroRelacionado3;
+	private String LibroRelacionado4;
+	private String LibroRelacionado5;
+	private String LibroRelacionado6;
+	private String LibroRelacionado7;
+	private String LibroRelacionado8;
+	
+	//IMAGEN DE LIBRO GRANDE
+	public String getImagenLibroG() {
+		return ImagenLibroG;
+	}
+	
+	public void setImagenLibroG(String imagenLibroG) {
+		ImagenLibroG = imagenLibroG;
+	}
+	
+	//LIBROS RELACIONADOS
+	public String getLibroRelacionado1() {
+        return LibroRelacionado1;
+    }
+    public void setLibroRelacionado1(String libroRelacionado1) {
+        LibroRelacionado1 = libroRelacionado1;
+    }
+    
+    public String getLibroRelacionado2() {
+        return LibroRelacionado2;
+    }
+    public void setLibroRelacionado2(String libroRelacionado2) {
+        LibroRelacionado2 = libroRelacionado2;
+    }
+    
+    public String getLibroRelacionado3() {
+        return LibroRelacionado3;
+    }
+    public void setLibroRelacionado3(String libroRelacionado3) {
+        LibroRelacionado3 = libroRelacionado3;
+    }
+    
+    public String getLibroRelacionado4() {
+        return LibroRelacionado4;
+    }
+    public void setLibroRelacionado4(String libroRelacionado4) {
+        LibroRelacionado4 = libroRelacionado4;
+    }
+    
+    public String getLibroRelacionado5() {
+        return LibroRelacionado5;
+    }
+    public void setLibroRelacionado5(String libroRelacionado5) {
+        LibroRelacionado5 = libroRelacionado5;
+    }
+    
+    public String getLibroRelacionado6() {
+        return LibroRelacionado6;
+    }
+    public void setLibroRelacionado6(String libroRelacionado6) {
+        LibroRelacionado6 = libroRelacionado6;
+    }
+    
+    public String getLibroRelacionado7() {
+        return LibroRelacionado7;
+    }
+    public void setLibroRelacionado7(String libroRelacionado7) {
+        LibroRelacionado7 = libroRelacionado7;
+    }
+    
+    public String getLibroRelacionado8() {
+        return LibroRelacionado8;
+    }
+    public void setLibroRelacionado8(String libroRelacionado8) {
+        LibroRelacionado8 = libroRelacionado8;
+    }
+
+}
